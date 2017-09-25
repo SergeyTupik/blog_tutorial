265b4f25903297496cf710bdc1261c4ab59d6954
SergeyTupik <pleomax3002@gmail.com>
2017-09-25 14:38:29 +0300

    -реструктурировал код, добавил ссылку на добавления нового поста авторизированному пользователю

--- resources/views/layouts/navbar-top.blade.php
+++ resources/views/layouts/navbar-top.blade.php
@@ -7,13 +7,13 @@
 
             @if(Auth::check())
                 <a class="nav-link ml-auto" href="#">{{ Auth::user()->name }}</a>
+                <a class="nav-link" href="{{ route('posts.create') }}">Добавить запись</a>
                 <a class="nav-link " href="{{ route('logout') }}">Выйти</a>
+            @else
+                <a class="nav-link ml-auto" href="{{ route('login') }}">Войти</a>
+                <a class="nav-link" href="{{ route('register') }}">Регистрация</a>
             @endif
 
-            @if(! Auth::check())
-                <a class="nav-link ml-auto" href="{{ route('login') }}">Войти</a>
-                <a class="nav-link" href="/register">Регистрация</a>
-            @endif
         </nav>
     </div>
 </div>
